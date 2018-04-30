ae9cc15f045fb364e873d2c0216e42813a78dffd
THOMAS SIMON STEINMANN <19591676@students.ltu.edu.au>
2018-04-30 13:58:19 +1000

    Saving progress

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ Created on Mon Apr  9 12:09:26 2018
 """
 
 import utils
+from random import randint
+from copy import deepcopy
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
@@ -44,49 +46,95 @@ trainingA = np.row_stack((training_data['t_2'], training_data['d'], training_dat
 
 
 """ Fuzzy membership function definitions """
-N_FUZZY_SUBSETS = 5
+TEMP_SUBSETS = 3
+DEMAND_SUBSETS = 5
+PRICE_SUBSETS = 5
+
+def names(num_names):
+    names = ('ultra low', 'very low', 'low', 'medium', 'high', 'very high', 'ultra high')
+    
+    if num_names == 7:
+        return list(names)
+    if num_names == 5:
+        return list(names[1:-1])
+    if num_names == 3:
+        return list(names[2:-2])
+    raise Exception('Illegal number of names')
 
 temp = ctrl.Antecedent(np.arange(0, max(training_data['t_2']), 1), 'Temperature')
+temp.automf(TEMP_SUBSETS, names=names(TEMP_SUBSETS))
 demand = ctrl.Antecedent(np.arange(0, max(training_data['d']), 1), 'Demand')
+demand.automf(DEMAND_SUBSETS, names=names(DEMAND_SUBSETS))
 price = ctrl.Consequent(np.arange(0, max(training_data['p']), 1), 'Price')
+price.automf(PRICE_SUBSETS, names=names(PRICE_SUBSETS))
 
 universes = [temp, demand, price]
 
-names = ['very low', 'low', 'medium', 'high', 'very high']
 print('Membership functions:')
 for universe in universes:
-    universe.automf(N_FUZZY_SUBSETS, names=names)
+    universe
     universe.view()
     
 """ Fuzzy rules definitions """
 # create all possible and rules
 rules = []
-for in_name1 in names:
-    for in_name2 in names:
-        for out_name in names:
-            rules.append(ctrl.Rule(temp[in_name1] | demand[in_name2], price[out_name]))
+for tmp_name in names(TEMP_SUBSETS):
+    for dem_name in names(DEMAND_SUBSETS):
+        for out_name in names(PRICE_SUBSETS):
+            rules.append(ctrl.Rule(temp[tmp_name] | demand[dem_name], price[out_name]))
 
-# Todo: Feature selection
-#   Add 2, remove 1, keep best performing combination
-
-""" while stop condition: """
-applied_rules = []
-available_rules = list(range(len(rules)))
-while stop_condition:
-    # pick two
-    
-    # drop one
-    
+# Todo: Feature selection by crossValidation
+""" Feature selection: Evolutionary approach """
+# Parameters
+TOTAL_FEATURES = TEMP_SUBSETS * DEMAND_SUBSETS * PRICE_SUBSETS
+ITERATIONS = TOTAL_FEATURES * 2 // 3
 
 
-# Structure: rule = ctrl.Rule(temp[0], price[4])
-tipping_ctrl = ctrl.ControlSystem(rules)
-simulation = ctrl.ControlSystemSimulation(tipping_ctrl)
+def evaluate_simulation(pop):
+    return randint(0, 100)
+    # TODO: implement
+    pass
+
 
 def build_simulation():
     # TODO: implement
     pass
 
-def evaluate_simulation():
-    # TODO: implement
-    pass
+
+def mutate(pop, index, direction):
+    """ Mutates(flips) each gene and evaluates it. Returns a tuple (accuracy, population) """
+    new_pop = deepcopy(pop)
+    
+    if new_pop[index] != direction:             # Mutate if not already active
+        new_pop[index] = direction
+        return evaluate_simulation(new_pop), new_pop
+    
+    return -1, None                             # Return -1 accuracy if mutation was not performed
+
+
+# Generate initial population
+population = [False for i in range(TOTAL_FEATURES)]
+best_population = evaluate_simulation(population), population
+
+for i in range(ITERATIONS):
+    # Pick two
+    for takes in range(2):
+        candidate = max([mutate(population, index, True) for index in range(len(population))])
+        # Save best performing
+        if candidate[0] > best_population[0]:
+            best_population = deepcopy(candidate)
+   
+    # Drop one
+    candidate = max([mutate(population, index, False) for index in range(len(population))])
+    # Save best performing
+    if candidate[0] > best_population[0]:
+        best_population = deepcopy(candidate)
+
+
+# Structure: rule = ctrl.Rule(temp[0], price[4])
+# tipping_ctrl = ctrl.ControlSystem(rules)
+# simulation = ctrl.ControlSystemSimulation(tipping_ctrl)
+
+print("Best Population:")
+print(best_population)
+
